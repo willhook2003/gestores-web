f816fb245898df8626ef7062ba7f1992df44b208
willhook2003@gmail.com <willhook2003@gmail.com>
2024-02-05 13:23:42 -0300

    act

--- src/app/(gestores)/agenda/page.tsx
+++ src/app/(gestores)/agenda/page.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from "@mui/material";
 export default function AgendaPage() {
   return (
     <Grid container justifyContent={"center"}>
-      <Typography variant="h5">Agenda</Typography>
+      <Typography variant="h5">Agenda Page</Typography>
     </Grid>
   );
 }
